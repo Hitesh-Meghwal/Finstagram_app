828b6f355c3c259a8a022aa233f110233d51a0ba
Hitesh_Meghwal <21sycshiteshmeghwal@gmail.com>
2024-06-28 11:10:56 +0530

    working on loginpage

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +10,85 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  double? _deviceWidth, _deviceHeight;
+
+  //maintaining the state of form
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _deviceHeight = MediaQuery.of(context).size.height;
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          body: Container(
+            padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.05),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  _titleWidget(),
+                  _loginForm(),
+                  _loginBtn()
+                ],
+              ),
+            ),
+          ),
+    ));
   }
+
+  Widget _titleWidget(){
+    return Text(
+      "Finstagram",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: _deviceWidth! * 0.09
+      ),
+    );
+  }
+
+  Widget _loginForm(){
+    return Container(
+      height: _deviceHeight! * 0.2 ,
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _emailTextField()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField(){
+    return TextFormField(
+
+    );
+  }
+
+  Widget _loginBtn(){
+    return MaterialButton(
+        minWidth: _deviceWidth! * 0.7,
+        height: _deviceHeight! * 0.06,
+        color: Colors.white24,
+        child: Text(
+          "Login",
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: _deviceWidth! * 0.06
+          ),
+        ),
+        onPressed: (){
+
+    });
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:finstagram_app/Pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       initialRoute: 'login',
       routes: {
         'login':(context) => LoginPage(),
